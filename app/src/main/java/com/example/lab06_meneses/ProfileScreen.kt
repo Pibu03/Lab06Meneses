62418edecef514d106d4d9d9f20e04d31026dd6a
Pibu03 <estivenramosallca@gmail.com>
2024-11-10 11:49:05 -0500

    Icons Commit
    Iconos más rutas de vistas por icono, y titulos con descripción por cada vista.

--- app/src/main/java/com/example/lab06_meneses/ProfileScreen.kt
+++ app/src/main/java/com/example/lab06_meneses/ProfileScreen.kt
@@ -2,20 +2,25 @@ package com.example.lab06_meneses
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ProfileScreen() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "User Profile")
-        //agregar contenido
+        Text("Pantalla de Favoritos", fontSize = 24.sp, textAlign = TextAlign.Center)
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id orci nec massa placerat ultricies.", textAlign = TextAlign.Center)
     }
 }
