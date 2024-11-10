74278ad722d3b654c16f56225c0a298c73ee2e75
Pibu03 <estivenramosallca@gmail.com>
2024-11-10 08:36:41 -0500

    CustomTopBar
    Actualización de CustomTopBar

--- app/src/main/java/com/example/lab06_meneses/MainActivity.kt
+++ app/src/main/java/com/example/lab06_meneses/MainActivity.kt
@@ -6,8 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,15 +27,64 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab06MenesesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CustomScaffold()
             }
         }
     }
+}
+
+@Composable
+fun CustomScaffold() {
+    Scaffold(
+        topBar = { CustomTopBar() },
+        bottomBar = { CustomBottomBar() },
+        floatingActionButton = { CustomFAB() },
+        content = { padding ->
+            CustomContent(Modifier.padding(padding))
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopBar() {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Rounded.Menu, contentDescription = null)
+            }
+        },
+        title = { Text(text = "Sample Title") },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Outlined.AccountCircle,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun CustomBottomBar() {
+    Text("Bottom Bar")
+}
+
+@Composable
+fun CustomFAB() {
+    Text("FAB")
+}
+
+@Composable
+fun CustomContent(modifier: Modifier = Modifier) {
+    Greeting(name = "Android", modifier = modifier)
 }
 
 @Composable
@@ -42,6 +99,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Lab06MenesesTheme {
-        Greeting("Android")
+        CustomScaffold()
     }
 }
